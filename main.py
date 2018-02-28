42977c92ca0eb290849f888cb1274b3b1acb9c6e
daniel <daniel.shougam@gmail.com>
2018-02-28 12:19:15 +1100

    refactoring almost complete

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ window_height = 720
 bg_color = (169,169,169)
 
 class character:
-    def __init__(self, x_start, y_start_):
+    def __init__(self, x_start, y_start, height, width):
         self.x_start = x_start
         self.y_start = y_start
+        self.height = height
+        self.width = width
         self.x_change = 0
         self.y_change = 4
         self.boost_counter = 0
@@ -24,26 +26,21 @@ class character:
         self.old_x = self.x
         self.falling = False
         self.boost = False
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
-xbox = character(100, 300)
-hatsune = character(1000, 300)
+xbox = character(100, 300, 50, 60)
+hatsune = character(1000, 300, 60, 60)
 
 # set game window stats
 game_display = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption('PCSoc is best soc')
 clock = pygame.time.Clock()
 
-pcsoc_width = 50
-pcsoc_height = 60
-pcsoc_rect = pygame.Rect(pcsoc_x, pcsoc_y, pcsoc_width, pcsoc_height)
-pcsoc_sprite = pygame.image.load('images/pcsoc_logo.png')
-pcsoc_sprite = pygame.transform.scale(pcsoc_sprite, (pcsoc_width, pcsoc_height))
+xbox.sprite = pygame.image.load('images/pcsoc_logo.png')
+xbox.sprite = pygame.transform.scale(xbox.sprite, (xbox.width, xbox.height))
 
-gamedev_width = 60
-gamedev_height = 60
-gamedev_rect = pygame.Rect(gamedev_x, gamedev_y, gamedev_width, gamedev_height)
-gamedev_sprite = pygame.image.load('images/gamedev_white.png')
-gamedev_sprite = pygame.transform.scale(gamedev_sprite, (gamedev_width, gamedev_height))
+hatsune.sprite = pygame.image.load('images/gamedev_white.png')
+hatsune.sprite = pygame.transform.scale(hatsune.sprite, (hatsune.width, hatsune.height))
 
 boundary_right_rect = pygame.Rect(1250, 0, 30, 720)
 boundary_left_rect = pygame.Rect(0, 0, 30, 720)
@@ -51,8 +48,10 @@ boundary_top_rect = pygame.Rect(0, 0, 1280, 30)
 boundary_bottom_rect = pygame.Rect(30, 700, 1280, 720)
 
 def level_one():
-    pcsoc_boost_counter += 1
-    gamedev_boost_counter += 1
+    global xbox
+    global hatsune
+    xbox.boost_counter += 1
+    hatsune.boost_counter += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -66,166 +65,166 @@ def level_one():
 
         #if event.type == pygame.JOYBUTTONDOWN:
         #    if joystick.get_button(0):
-        #        pcsoc_y_change = -4
+        #        xbox.y_change = -4
 
         #if event.type == pygame.JOYBUTTONUP:
         #    if not joystick.get_button(0):
-        #        pcsoc_y_change = 0
+        #        xbox.y_change = 0
 
         # key presses
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
-                pcsoc_x_change = -4
+                xbox.x_change = -4
             elif event.key == pygame.K_d:
-                pcsoc_x_change = 4
+                xbox.x_change = 4
             elif event.key == pygame.K_w:
-                pcsoc_y_change = -5
+                xbox.y_change = -5
             elif event.key == pygame.K_s:
-                pcsoc_y_change = 9
-                pcsoc_falling = True
+                xbox.y_change = 9
+                xbox.falling = True
             elif event.key == pygame.K_LSHIFT:
-                if pcsoc_boost_counter > 300:
-                    pcsoc_direction = pcsoc_x_change
-                    pcsoc_old_x = pcsoc_x
-                    pcsoc_boost = True
+                if xbox.boost_counter > 300:
+                    xbox.direction = xbox.x_change
+                    xbox.old_x = xbox.x
+                    xbox.boost = True
             elif event.key == pygame.K_RSHIFT:
-                if gamedev_boost_counter > 300:
-                    gamedev_direction = gamedev_x_change
-                    gamedev_old_x = gamedev_x
-                    gamedev_boost = True
+                if hatsune.boost_counter > 300:
+                    hatsune.direction = hatsune.x_change
+                    hatsune.old_x = hatsune.x
+                    hatsune.boost = True
             elif event.key == pygame.K_LEFT:
-                gamedev_x_change = -4
+                hatsune.x_change = -4
             elif event.key == pygame.K_RIGHT:
-                gamedev_x_change = 4
+                hatsune.x_change = 4
             elif event.key == pygame.K_UP:
-                gamedev_y_change = -5
+                hatsune.y_change = -5
             elif event.key == pygame.K_DOWN:
-                gamedev_y_change = 9
-                gamedev_falling = True
+                hatsune.y_change = 9
+                hatsune.falling = True
 
         # key releases
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a or event.key == pygame.K_d:
-                pcsoc_x_change = 0
+                xbox.x_change = 0
             if event.key == pygame.K_s or event.key == pygame.K_w:
-                pcsoc_y_change = 4
+                xbox.y_change = 4
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-               gamedev_x_change = 0
+               hatsune.x_change = 0
             if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
-               gamedev_y_change = 4
+               hatsune.y_change = 4
 
-    if pcsoc_falling:
-        if pcsoc_rect.colliderect(boundary_bottom_rect):
-            pcsoc_y_change = 4
-            pcsoc_falling = False
+    if xbox.falling:
+        if xbox.rect.colliderect(boundary_bottom_rect):
+            xbox.y_change = 4
+            xbox.falling = False
         else:
-            pcsoc_y_change = 9
+            xbox.y_change = 9
 
-    if pcsoc_boost:
-        if pcsoc_direction > 0:
-            if pcsoc_x < pcsoc_old_x + 90:
-                pcsoc_x_change = 10
+    if xbox.boost:
+        if xbox.direction > 0:
+            if xbox.x < xbox.old_x + 90:
+                xbox.x_change = 10
             else:
-                pcsoc_x_change = 4
-                pcsoc_boost = False
-                pcsoc_boost_counter = 0
+                xbox.x_change = 4
+                xbox.boost = False
+                xbox.boost_counter = 0
         else:
-            if pcsoc_x > pcsoc_old_x - 90:
-                pcsoc_x_change = - 10
+            if xbox.x > xbox.old_x - 90:
+                xbox.x_change = - 10
             else:
-                pcsoc_x_change = -4
-                pcsoc_boost = False
-                pcsoc_boost_counter = 0
+                xbox.x_change = -4
+                xbox.boost = False
+                xbox.boost_counter = 0
 
-    if gamedev_falling:
-        if gamedev_rect.colliderect(boundary_bottom_rect):
-            gamedev_y_change = 4
-            gamedev_falling = False
+    if hatsune.falling:
+        if hatsune.rect.colliderect(boundary_bottom_rect):
+            hatsune.y_change = 4
+            hatsune.falling = False
         else:
-            gamedev_y_change = 9
+            hatsune.y_change = 9
             
-    if gamedev_boost:
-        if gamedev_direction > 0:
-            if gamedev_x < gamedev_old_x + 90:
-                gamedev_x_change = 10
+    if hatsune.boost:
+        if hatsune.direction > 0:
+            if hatsune.x < hatsune.old_x + 90:
+                hatsune.x_change = 10
             else:
-                gamedev_x_change = 4
-                gamedev_boost = False
-                gamedev_boost_counter = 0
+                hatsune.x_change = 4
+                hatsune.boost = False
+                hatsune.boost_counter = 0
         else:
-            if gamedev_x > gamedev_old_x - 90:
-                gamedev_x_change = - 10
+            if hatsune.x > hatsune.old_x - 90:
+                hatsune.x_change = - 10
             else:
-                gamedev_x_change = -4
-                gamedev_boost = False
-                gamedev_boost_counter = 0
+                hatsune.x_change = -4
+                hatsune.boost = False
+                hatsune.boost_counter = 0
 
     # boundary collisions
-    if ((pcsoc_rect.colliderect(boundary_right_rect) and pcsoc_x_change > 0) or
-    (pcsoc_rect.colliderect(boundary_left_rect) and pcsoc_x_change < 0)):
-        pcsoc_x_change = 0
+    if ((xbox.rect.colliderect(boundary_right_rect) and xbox.x_change > 0) or
+    (xbox.rect.colliderect(boundary_left_rect) and xbox.x_change < 0)):
+        xbox.x_change = 0
 
-    if ((pcsoc_rect.colliderect(boundary_top_rect) and pcsoc_y_change < 0) or
-        (pcsoc_rect.colliderect(boundary_bottom_rect) and pcsoc_y_change > 0)):
-        pcsoc_y_change = 0
+    if ((xbox.rect.colliderect(boundary_top_rect) and xbox.y_change < 0) or
+        (xbox.rect.colliderect(boundary_bottom_rect) and xbox.y_change > 0)):
+        xbox.y_change = 0
 
-    if ((gamedev_rect.colliderect(boundary_right_rect) and gamedev_x_change > 0) or
-        (gamedev_rect.colliderect(boundary_left_rect) and gamedev_x_change < 0)):
-        gamedev_x_change = 0
+    if ((hatsune.rect.colliderect(boundary_right_rect) and hatsune.x_change > 0) or
+        (hatsune.rect.colliderect(boundary_left_rect) and hatsune.x_change < 0)):
+        hatsune.x_change = 0
 
-    if ((gamedev_rect.colliderect(boundary_top_rect) and gamedev_y_change < 0) or
-        (gamedev_rect.colliderect(boundary_bottom_rect) and gamedev_y_change > 0)):
-        gamedev_y_change = 0
+    if ((hatsune.rect.colliderect(boundary_top_rect) and hatsune.y_change < 0) or
+        (hatsune.rect.colliderect(boundary_bottom_rect) and hatsune.y_change > 0)):
+        hatsune.y_change = 0
 
     # character collisions
-    if (pcsoc_rect.colliderect(gamedev_rect)):
-        if pcsoc_x_change > 0 and pcsoc_y in range(int(gamedev_y - gamedev_height / 2), int(gamedev_y + gamedev_height / 2)):
-            if pcsoc_x < gamedev_x:
-                pcsoc_x_change = 0
-        if pcsoc_x_change < 0 and pcsoc_y in range(int(gamedev_y - gamedev_height / 2), int(gamedev_y + gamedev_height / 2)):
-            if pcsoc_x > gamedev_x:
-                pcsoc_x_change = 0
-        if (pcsoc_x in range(int(gamedev_x - gamedev_width / 2), int(gamedev_x + gamedev_width / 2)) and pcsoc_y < int(gamedev_y + gamedev_height / 2)):
-            pcsoc_y_change = 0
+    if (xbox.rect.colliderect(hatsune.rect)):
+        if xbox.x_change > 0 and xbox.y in range(int(hatsune.y - hatsune.height / 2), int(hatsune.y + hatsune.height / 2)):
+            if xbox.x < hatsune.x:
+                xbox.x_change = 0
+        if xbox.x_change < 0 and xbox.y in range(int(hatsune.y - hatsune.height / 2), int(hatsune.y + hatsune.height / 2)):
+            if xbox.x > hatsune.x:
+                xbox.x_change = 0
+        if (xbox.x in range(int(hatsune.x - hatsune.width / 2), int(hatsune.x + hatsune.width / 2)) and xbox.y < int(hatsune.y + hatsune.height / 2)):
+            xbox.y_change = 0
             
-        if gamedev_x_change > 0 and gamedev_y in range(int(pcsoc_y - pcsoc_height / 2), int(pcsoc_y + pcsoc_height / 2)):
-            if gamedev_x < pcsoc_x:
-                gamedev_x_change = 0
-        if gamedev_x_change < 0 and gamedev_y in range(int(pcsoc_y - pcsoc_height / 2), int(pcsoc_y + pcsoc_height / 2)):
-            if gamedev_x > pcsoc_x:
-                gamedev_x_change = 0
-        if (gamedev_x in range(int(pcsoc_x - pcsoc_width / 2), int(pcsoc_x + pcsoc_width / 2)) and gamedev_y < int(pcsoc_y + pcsoc_height / 2)):
-            gamedev_y_change = 0
+        if hatsune.x_change > 0 and hatsune.y in range(int(xbox.y - xbox.height / 2), int(xbox.y + xbox.height / 2)):
+            if hatsune.x < xbox.x:
+                hatsune.x_change = 0
+        if hatsune.x_change < 0 and hatsune.y in range(int(xbox.y - xbox.height / 2), int(xbox.y + xbox.height / 2)):
+            if hatsune.x > xbox.x:
+                hatsune.x_change = 0
+        if (hatsune.x in range(int(xbox.x - xbox.width / 2), int(xbox.x + xbox.width / 2)) and hatsune.y < int(xbox.y + xbox.height / 2)):
+            hatsune.y_change = 0
 
     # check if one player on top of the other
-    if (pcsoc_rect.colliderect(gamedev_rect)):
-        if ((pcsoc_y + pcsoc_height / 2) in range(int(gamedev_y - gamedev_height / 2), int(gamedev_y)) and
-            (pcsoc_x in range(int(gamedev_x - gamedev_width / 2), int(gamedev_x + gamedev_width / 2)))):
-            gamedev_rect = pygame.Rect(gamedev_x_start, gamedev_y_start, gamedev_width, gamedev_height)
-            gamedev_x = gamedev_x_start
-            gamedev_y  = gamedev_y_start
-            pcsoc_y_change = 4
-            gamedev_y_change = 4
+    if (xbox.rect.colliderect(hatsune.rect)):
+        if ((xbox.y + xbox.height / 2) in range(int(hatsune.y - hatsune.height / 2), int(hatsune.y)) and
+            (xbox.x in range(int(hatsune.x - hatsune.width / 2), int(hatsune.x + hatsune.width / 2)))):
+            hatsune.rect = pygame.Rect(hatsune.x_start, hatsune.y_start, hatsune.width, hatsune.height)
+            hatsune.x = hatsune.x_start
+            hatsune.y  = hatsune.y_start
+            xbox.y_change = 4
+            hatsune.y_change = 4
 
-    if (gamedev_rect.colliderect(pcsoc_rect)):
-        if ((gamedev_y + gamedev_height / 2) in range(int(pcsoc_y - pcsoc_height / 2), int(pcsoc_y)) and
-            (gamedev_x in range(int(pcsoc_x - pcsoc_width / 2), int(pcsoc_x + pcsoc_width / 2)))):
-            pcsoc_rect = pygame.Rect(pcsoc_x_start, pcsoc_y_start, pcsoc_width, pcsoc_height)
-            pcsoc_x = pcsoc_x_start
-            pcsoc_y  = pcsoc_y_start
-            gamedev_y_change = 4
-            pcsoc_y_change = 4 
+    if (hatsune.rect.colliderect(xbox.rect)):
+        if ((hatsune.y + hatsune.height / 2) in range(int(xbox.y - xbox.height / 2), int(xbox.y)) and
+            (hatsune.x in range(int(xbox.x - xbox.width / 2), int(xbox.x + xbox.width / 2)))):
+            xbox.rect = pygame.Rect(xbox.x_start, xbox.y_start, xbox.width, xbox.height)
+            xbox.x = xbox.x_start
+            xbox.y  = xbox.y_start
+            hatsune.y_change = 4
+            xbox.y_change = 4 
 
-    pcsoc_rect.move_ip(pcsoc_x_change, pcsoc_y_change)
-    gamedev_rect.move_ip(gamedev_x_change, gamedev_y_change)
-    pcsoc_x += pcsoc_x_change
-    pcsoc_y += pcsoc_y_change
-    gamedev_x += gamedev_x_change
-    gamedev_y += gamedev_y_change
+    xbox.rect.move_ip(xbox.x_change, xbox.y_change)
+    hatsune.rect.move_ip(hatsune.x_change, hatsune.y_change)
+    xbox.x += xbox.x_change
+    xbox.y += xbox.y_change
+    hatsune.x += hatsune.x_change
+    hatsune.y += hatsune.y_change
   
     game_display.fill(bg_color)
-    game_display.blit(pcsoc_sprite, pcsoc_rect)
-    game_display.blit(gamedev_sprite, gamedev_rect)
+    game_display.blit(xbox.sprite, xbox.rect)
+    game_display.blit(hatsune.sprite, hatsune.rect)
     pygame.display.update() 
     clock.tick(60) # 60 FPS
     
